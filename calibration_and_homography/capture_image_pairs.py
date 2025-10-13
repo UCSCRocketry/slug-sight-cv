11c844b350c2e9cb0cf4aa62c9da8d11d3c015a7
danepearson <114421824+danepearson@users.noreply.github.com>
2025-10-13 11:22:52 -0700

    Added comments for clarity.

--- calibration_and_homography/capture_image_pairs.py
+++ calibration_and_homography/capture_image_pairs.py
@@ -3,19 +3,20 @@ import cv2
 import time
 import os
 
-os.makedirs("calib_images", exist_ok=True)
+os.makedirs("calib_images", exist_ok=True)  # If the folder calib_images doesn’t exist, it will be created
 
 cam0 = Picamera2(0)
 cam1 = Picamera2(1)
+# Start capturing frames internally so they’re ready when I ask for an image
 cam0.start()
 cam1.start()
 
 for i in range(10):  # capture 10 pairs
-    frame0 = cam0.capture_array()
-    frame1 = cam1.capture_array()
-    cv2.imwrite(f"calib_images/left_{i:02d}.jpg", frame0)
+    frame0 = cam0.capture_array()  # Returns a NumPy array of image data
+    frame1 = cam1.capture_array()  # same thing
+    cv2.imwrite(f"calib_images/left_{i:02d}.jpg", frame0)  
     cv2.imwrite(f"calib_images/right_{i:02d}.jpg", frame1)
     print(f"Captured pair {i}")
-    time.sleep(2)  # move checkerboard slightly each time
+    time.sleep(2)
 
 print("All pairs captured.")
